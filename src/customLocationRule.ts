9a66234e208a13c712c41bc11511618e83a12324
Mitch Lloyd <mitch.lloyd@gmail.com>
2017-04-23 16:04:44 -0700

    Add customLocation implementation with checker

--- src/customLocationRule.ts
+++ src/customLocationRule.ts
@@ -11,4 +11,19 @@ export class Rule extends Lint.Rules.TypedRule {
 }
 
 class CustomLocationWalker extends Lint.ProgramAwareRuleWalker {
+  public visitIdentifier(node) {
+    if (node.text === 'location') {
+      // This is the TypeScript type checker!
+      // https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
+      const checker = this.getTypeChecker();
+
+      const type = checker.getTypeAtLocation(node);
+
+      if (checker.typeToString(type) === 'Location') {
+        this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+      }
+    }
+
+    // No super because identifiers don't have children.
+  }
 }
--- src/demo/utils/my-utility.ts
+++ src/demo/utils/my-utility.ts
@@ -1,1 +1,5 @@
 import queryParams from 'parse-query-params';
+
+// Where's my squiggles?
+// https://github.com/Microsoft/vscode-tslint/issues/171
+location.href = 'google.com';
